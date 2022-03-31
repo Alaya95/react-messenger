d4ce37d981ef1b8fa7e68a7633546a68adef9271
Elena <rozetta.95@mail.ru>
2022-03-31 13:18:22 +0300

    add chats

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
 import Form from './components/Form/Form';
-import { useState, useEffect } from 'react';
+import { ChatsList } from './components/ChatsList/ChatsList';
+import { useState, useRef, useEffect } from 'react';
 import { AUTHORS } from './components/utils/constants';
 import { MessageList } from './components/MessageList/MessageList';
+import './App.css';
+import { Container } from '@mui/material';
+
+const chats = [
+  { id: 1, name: 'aria', family: 'Inca' },
+  { id: 2, name: 'eli', family: 'Ban' },
+  { id: 3, name: 'Anna', family: 'Smith' },
+  { id: 4, name: 'Alex', family: 'Bran' },
+];
 
 function App() {
   const [messages, setMessages] = useState([]);
-
+  const timeout = useRef();
   const addMessage = (message) => {
     setMessages([...messages, message]);
   };
-
   const sendMessages = (text) => {
     addMessage({
+      id: Date.now(),
       author: AUTHORS.human,
       text,
     });
   };
 
   useEffect(() => {
-    let timeout;
     if (messages[messages.length - 1]?.author === AUTHORS.human) {
-      timeout = setTimeout(() => {
-        addMessage({ author: AUTHORS.robot, text: 'fdsfsd' });
+      timeout.current = setTimeout(() => {
+        addMessage({ author: AUTHORS.robot, text: 'fdsfsd', id: Date.now() });
       }, 1000);
     }
     return () => {
-      clearTimeout(timeout);
+      clearTimeout(timeout.current);
     };
   }, [messages]);
 
   return (
     <div className="App">
-      <MessageList messages={messages} />
-      <Form onSubmit={sendMessages} />
+      <Container maxWidth="sm">
+        <ChatsList chats={chats} />
+        <MessageList messages={messages} />
+        <Form onSubmit={sendMessages} />
+      </Container>
     </div>
   );
 }
