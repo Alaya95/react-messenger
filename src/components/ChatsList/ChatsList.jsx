60963e8dcb8f55399e95ac9741e81eaaf5580623
Elena <rozetta.95@mail.ru>
2022-04-03 22:20:23 +0300

    edit

--- src/components/ChatsList/ChatsList.jsx
+++ src/components/ChatsList/ChatsList.jsx
@@ -1,20 +1,40 @@
 import PropTypes from 'prop-types';
-import { List } from '@mui/material';
+import { Box, List } from '@mui/material';
 import { Chat } from '../Chat/Chat';
-export const ChatsList = ({ chats }) => {
+import { NavLink, Outlet } from 'react-router-dom';
+import './ChatList.styles.css';
+
+const chats = [
+    { id: 'chat1', name: 'aria', family: 'Inca' },
+    { id: 'chat2', name: 'eli', family: 'Ban' },
+    { id: 'chat3', name: 'Anna', family: 'Smith' },
+    { id: 'chat4', name: 'Alex', family: 'Bran' },
+];
+
+export const ChatsList = () => {
 
     return (
-        <List>
-            {chats.map((chat) => (
-                <Chat
-                    key={chat.id}
-                    username={chat.name}
-                    family={chat.family}
-                />
-            ))}
-        </List >
+        <Box
+            sx={{
+                height: '90%',
+                display: 'grid',
+                gridTemplateColumns: 'repeat(4, 1fr)',
+                gap: 1,
+            }}>
+            <List sx={{ gridColumn: '1 / 2', boxShadow: '-10px 0px 24px grey', }}>
+                {chats.map((chat) => (
+                    <NavLink className='chatlist-link' to={`/chat/${chat.id}`} key={chat.id}>
+                        <Chat
+                            key={chat.id}
+                            username={chat.name}
+                            family={chat.family}
+                        />
+                    </NavLink>
+                ))}
+            </List >
+            <Outlet />
+        </Box>
     )
-
 }
 
 ChatsList.propTypes = {
