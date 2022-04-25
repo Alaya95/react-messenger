354d87e9217a560448b6894ad122607c860298b7
Elena <rozetta.95@mail.ru>
2022-04-25 10:48:11 +0300

    profile.

--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -1,57 +1,39 @@
 // import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Form from "../../components/Form/Form";
 import { logOut } from "../../services/firebase";
-import { setName, toggleCheckbox } from "../../store/profile/actions";
+import { initProfileTrack, setNameFB, setShowName, stopProfileTrack } from "../../store/profile/actions";
 import { selectName, selectShowName } from "../../store/profile/selectors";
 
-//версия с коннект
-// const ProfileToConnect = ({ name, showName, changeName, changeCheckbox }) => {
-//     const handleClick = () => {
-//         changeCheckbox(showName)
-//     };
-//     const handleSubmit = (text) => {
-//         changeName(text)
-//     };
-//     return (
-//         <>
-//             <div>Profile</div>
-//             <div>
-//                 <button onClick={handleClick} >{showName ? 'off' : 'on'}</button>
-//                 {showName && <p>{name}</p>}
-//             </div>
-//             <Form onSubmit={handleSubmit} />
-//         </>
-//     );
-// }
-
-// const mapStateToProps = state => ({
-//     name: state.profile.name,
-//     showName: state.profile.showName
-// });
-
-// const mapDispatch = {
-//     changeName: setName,
-//     changeCheckbox: () => toggleCheckbox,
-// }
-
-// export const Profile = connect(mapStateToProps, mapDispatch)(ProfileToConnect);
-
-//версия с селектором
 export const Profile = ({ onLogout }) => {
     const dispatch = useDispatch();
     const name = useSelector(selectName);
     const showName = useSelector(selectShowName);
 
-    const handleClick = () => { dispatch(toggleCheckbox) };
-    const handleSubmit = (text) => { dispatch(setName(text)) };
+    const handleClick = () => {
+        dispatch(setShowName(!showName));
+    };
+    const handleSubmit = (text) => {
+        dispatch(setNameFB(text));
+    };
+
+    useEffect(() => {
+        dispatch(initProfileTrack());
+
+        return () => {
+            dispatch(stopProfileTrack());
+        };
+    }, []);
 
     return (
         <>
             <div>Profile</div>
             <button onClick={logOut}>Logout</button>
             <div>
-                <button onClick={handleClick} > {showName ? 'off' : 'on'}</button>
+                <button onClick={handleClick} >
+                    {showName ? 'off' : 'on'}
+                </button>
                 {showName && <p>{name}</p>}
             </div>
             <Form onSubmit={handleSubmit} />
